d12bb18434ffca0352fdb340209ea393e4c66a06
matheus camargo <matheusgcamargo@hotmail.com>
2022-05-28 11:25:21 -0300

    implemented basic controller and views

--- app/controllers/groceries_lists_controller.rb
+++
@@ -1,2 +0,0 @@
-class GroceriesListsController < ApplicationController
-end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,14 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :users, only: [:show] do
+  # resources :users, only: [:show] do
+  #   resources :meals, only: [ :index, :show ]
+  #   resources :groceries_lists, only: [:show]
+  # end
+
+  get "/profile", to: "pages#profile"
+
+  namespace :profile do
     resources :meals, only: [ :index, :show ]
     resources :groceries_lists, only: [:show]
   end
