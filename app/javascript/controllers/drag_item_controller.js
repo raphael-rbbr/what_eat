02dd7c1e1d01463d25e8838f6cae3778e91455ce
matheus camargo <matheusgcamargo@hotmail.com>
2022-06-03 17:59:35 -0300

    implemented link between timetable and meals index - pending: (i) save list -as is- and (ii) order does not mess list

--- app/javascript/controllers/drag_item_controller.js
+++ app/javascript/controllers/drag_item_controller.js
@@ -5,42 +5,15 @@ export default class extends Controller {
 
   connect() {
     this.sortable = Sortable.create(this.element, {
-      onEnd:this.end.bind(this)
+      onEnd:this.end.bind(this),
+      group: 'shared',
+      pull: 'clone',
+      animation: 150
     })
   }
 
   end(event) {
     console.log(event)
   }
-
-  // dragstart(event) {
-  //   event.dataTransfer.setData("application/drag-key", event.target.getAttribute("data-todo-id"))
-  //   event.dataTransfer.effectAllowed = "move"
-  // }
-
-  // dragover(event) {
-  //   event.preventDefault()
-  //   return true
-  // }
-
-  // dragenter(event) {
-  //     event.preventDefault()
-  // }
-
-  // drop(event) {
-  //   var data = event.dataTransfer.getData("application/drag-key")
-  //   const dropTarget = event.target
-  //   const draggedItem = this.element.querySelector(`[data-todo-id='${data}']`);
-  //   const positionComparison = dropTarget.compareDocumentPosition(draggedItem)
-  //   if ( positionComparison & draggedItem.id + 1 ) {
-  //       event.target.insertAdjacentElement('beforebegin', draggedItem);
-  //   } else if ( positionComparison & draggedItem.id - 1) {
-  //       event.target.insertAdjacentElement('afterend', draggedItem);
-  //   }
-  //   event.preventDefault()
-  // }
-
-  // dragend(event) {
-  // }
 
 }
