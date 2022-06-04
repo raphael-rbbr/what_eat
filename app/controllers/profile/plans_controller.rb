44fbae2a2cadb5569a0a6b14104840cf18bd309d
matheus camargo <matheusgcamargo@hotmail.com>
2022-06-04 15:08:20 -0300

    improvred the drag and drop feature - no more resorting list on timetable

--- app/controllers/profile/plans_controller.rb
+++ app/controllers/profile/plans_controller.rb
@@ -1,9 +1,10 @@
 class Profile::PlansController < ApplicationController
-  efore_action :set_user
+  before_action :set_user
   # before_action :set_recipe
 
   def index
-    @plan = Plan.where(:start_date < Date.now < :final_date)
+    @user.plans
+    # @plan = Plan.where(:start_date < Date.now < :final_date)
   end
 
   def new
