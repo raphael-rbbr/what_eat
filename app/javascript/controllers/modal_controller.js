2e8d92b15bb3970e1fb7faa037f6b66f6cac84f3
Gabriela Leite <nleite.gabriela@gmail.com>
2022-06-11 15:44:22 -0300

    show inside modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -3,22 +3,17 @@ import { useDispatch } from 'stimulus-use'
 
 export default class extends Controller {
   connect() {
-    console.log(this.modalTarget);
   }
 
   open(event) {
     const id = event.detail.content;
     const url = `/profile/recipes/${id}`
+    const recipe = document.getElementById(`recipe${id}`)
     fetch(url, { headers: { Accept: 'text/plain' } })
       .then((response) => response.text())
       .then((data) => {
-        const recipe = document.getElementById(`recipe${id}`)
         recipe.outerHTML=data
       })
-
-
-
-    // console.log(recipe.outerHTML);
   }
 
 }
