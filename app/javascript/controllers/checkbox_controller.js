102608f4db92ef59b56f2628124fc306db783525
Raphael Resende Breitschaft <96351830+raphael-rbbr@users.noreply.github.com>
2022-06-16 21:01:21 -0300

    Delete checkbox_controller.js

--- app/javascript/controllers/checkbox_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "stimulus"
-
-export default class extends Controller {
-    connect() {
-      // this.element.splice(2, 1, "checked");
-      console.log("Hello, Stimulus!", this.element)
-    }
-  }
